ed606709b165c7cb02d05fa3632bc9925a0ea934
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-06-24 18:03:44 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,10 @@ app.use(express.json());
 const PORT = 4000;
 
 const cors = require('cors');
-const allowedOrigins = [
-  'https://crm-tau-eight.vercel.app',
-  'https://crm-qyaz.vercel.app'
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // if you're using cookies or auth headers
+  origin: allowedOrigins,
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
 
 
