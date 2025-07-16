2243e7c46ea5567c299857a14d0ee5be72e3b016
ayoub agbalou <ayoubagbalou4@gmail.com>
2025-07-16 16:17:10 +0100

    v 3.0

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
-
+const mongoose = require('mongoose');
 const PORT = 4000;
-
 const cors = require('cors');
 
 app.use(cors({
@@ -17,20 +16,14 @@ app.use(cors({
 const dotenv = require('dotenv');
 dotenv.config()
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASEURL, {
-    serverSelectionTimeoutMS: 30000, 
-    socketTimeoutMS: 45000,
-    maxPoolSize: 50
-}).then(() => {
-    console.log("Successfully connected to MongoDB");
-}).catch((error) => {
-    console.error("Failed to connect to MongoDB", error);
-});
+mongoose.connect(process.env.DATABASEURL)
+    .then(() => {
+        console.log("Successfully connected to MongoDB");
+    }).catch((error) => {
+        console.error("Failed to connect to MongoDB", error);
+    });
 
-app.get('/', (req, res) => {
-    return res.json('App Is Run')
-})
+
 
 
 app.use('/', require('./routes/authRoutes'))
@@ -42,6 +35,9 @@ app.use('/', require("./routes/paymentSettingsRoutes"));
 app.use('/', require("./routes/notificationsRoutes"));
 app.use('/', require("./routes/emailSettingsRoutes"));
 
+app.get('/', (req, res) => {
+    return res.json('App Is Run')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
